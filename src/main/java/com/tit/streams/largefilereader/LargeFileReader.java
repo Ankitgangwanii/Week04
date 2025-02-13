a94a3694da3a96a6858d2f6978a84aea63edb819
Ankit Gangwani <Ankitgangwani160@gmail.com>
2025-02-13 13:35:25 +0530

    [Ankit] Add . Read a Large File Line by Line

---
+++ src/main/java/com/tit/streams/largefilereader/LargeFileReader.java
@@ -0,0 +1,20 @@
+package com.tit.streams.largefilereader;
+
+import java.io.*;
+
+public class LargeFileReader {
+    public static void main(String[] args) {
+        String filePath = "C:\\Users\\ankitgangwani\\Desktop\\Week04\\Day03\\source.txt";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.toLowerCase().contains("error")) {
+                    System.out.println(line);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
+    }
+}
